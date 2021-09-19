30d55ef86f56a8090b337508f7f6b6be8ad2d2cc
majidalimughal <mughalmajidali101@outlook.com>
2021-09-19 07:45:07 +0500

    Test

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -42,12 +42,12 @@ class Handler extends ExceptionHandler
 
     public function render($request, Throwable $e)
     {
-        // if ($e instanceof MissingShopDomainException) {
-        //     // dd($e);
-        //     return redirect()->route('login');
-        //     return response()->view('welcome', [], 200);
-        //     // parent::report($e);
-        // }
+        if ($e instanceof MissingShopDomainException) {
+            // dd($e);
+            return redirect()->route('login');
+            return response()->view('welcome', [], 200);
+            // parent::report($e);
+        }
         parent::report($e);
     }
 }
